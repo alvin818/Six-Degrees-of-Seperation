7c453258fecba97ce0f16dd029854111e2f45b0a
Alvin Vanegas <ajvanega@ucsd.edu>
2017-03-14 23:37:20 -0700

    khgkljjhjhkhj

--- ActorGraph.cpp
+++ ActorGraph.cpp
@@ -878,12 +878,15 @@ bool ActorGraph::BFSearchAC(string startActor, string actorToFind){
 
 // Destructor
 ActorGraph::~ActorGraph(){
-
+	cout << "Freeing memory!" << endl;
 	// free memory in both hashmaps
 	for (auto it = movies_map.begin(); it != movies_map.end(); it++){
 		delete it->second;
 	}
+	unordered_map<string, ActorNode*> tempMap;
+	actorNode_map.swap(tempMap);
 
+	/*
 	for (auto it = actorNode_map.begin(); it != actorNode_map.end(); it++){
 
 		for (auto it2 = it->second->movieEdges.begin(); it2 != it->second->movieEdges.end(); it2++){
@@ -891,6 +894,7 @@ ActorGraph::~ActorGraph(){
 		}
 		delete it->second;
 	}
+	*/
 }
 
 
